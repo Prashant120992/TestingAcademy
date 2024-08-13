4496aac6aa49ca65563a23078818d6bf363ac9d2
Your Name <you@example.com>
2024-08-13 13:11:45 +0530

    Added Task 2

---
+++ 13082024/Task02.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the num1 : "))
+num2 = int(input("Enter the num2 : "))
+print(f"The maximum number between {num1} and {num2} is", max(num1, num2))
+print(f"The Power of {num1} and {num2} is", pow(num1, num2))
+sub = num1 - num2
+print(f"The subtraction value of {num1} and {num2} is", sub)
+mul = num1 * num2
+print(f"The multiplication value of {num1} and {num2} is", mul)
+add = num1 + num2
+print(f"The addition value of {num1} and {num2} is", add)
+div = num1/num2
+print(f"The division value of {num1} and {num2} is", div)
