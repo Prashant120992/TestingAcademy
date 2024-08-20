972be19150257c267b6f2efff8cd0fbd7af09da0
Your Name <you@example.com>
2024-08-20 12:02:36 +0530

    Added Task8

---
+++ 20082024/Task08.py
@@ -0,0 +1,14 @@
+angle1 = int(input("Enter the angle1\n"))
+angle2 = int(input("Enter the angle2\n"))
+angle3 = int(input("Enter the angle3\n"))
+
+if angle1 == angle2 and angle1 == angle3:
+    print("The Triangle is equilateral")
+elif angle1 == angle2 and angle1 != angle3:
+    print("The Triangle is isosceles")
+elif angle1 == angle3 and angle1 != angle2:
+    print("The Triangle is isosceles")
+elif angle2 == angle3 and angle2 != angle1:
+    print("The Triangle is isosceles")
+else:
+    print("The Triangle is scalene ")
