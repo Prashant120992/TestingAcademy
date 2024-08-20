7d645fc00e04452afd5496155967e716e8665a9b
Your Name <you@example.com>
2024-08-20 15:03:44 +0530

    Added Task 9

---
+++ 20082024/Task09.py
@@ -0,0 +1,12 @@
+# Write a program that prints numbers from 1 to 100. # Loop For
+# However, for multiples of 3, print "Fizz" instead of the number, and for multiples of 5, print "Buzz."
+# For numbers that are multiples of both 3 and 5, print "FizzBuzz."
+for fizzbuzz in range(100):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 != 0:
+        print("fizz")
+    elif fizzbuzz % 5 == 0 and fizzbuzz % 3 != 0:
+        print("buzz")
+    elif fizzbuzz % 5 == 0 and fizzbuzz % 3 == 0:
+        print("fizzbuzz")
+    else:
+        print(fizzbuzz)
