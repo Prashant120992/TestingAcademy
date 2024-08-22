743bbcb5fd9db45277f62e890c59f32ba2edf342
Your Name <you@example.com>
2024-08-22 15:53:46 +0530

    Added Task 10

---
+++ 22082024/Task10.py
@@ -0,0 +1,10 @@
+number = int(input("Enter the number\n"))
+fact = 1
+if number < 0:
+    print("Negative numbers are not allowed")
+elif number == 1:
+    print("The factorial of 1 is 1")
+else:
+    for i in range(1, number + 1):
+        fact = fact * i
+    print("The factorial of", number, "is", fact)
