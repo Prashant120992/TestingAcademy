059832f11481d4e6aa915280e502968a815ca17d
Your Name <you@example.com>
2024-08-24 08:32:15 +0530

    Added Task 10 Edited the Logic for 0 Value

--- 22082024/Task10.py
+++ 22082024/Task10.py
@@ -2,8 +2,8 @@ number = int(input("Enter the integer number\n"))
 fact = 1
 if number < 0:
     print("Negative numbers are not allowed")
-elif number == 1:
-    print("The factorial of 1 is 1")
+elif number == 1 and number == 0:
+    print("The factorial of 1 and 0 is 1")
 else:
     for i in range(1, number + 1):
         fact = fact * i
